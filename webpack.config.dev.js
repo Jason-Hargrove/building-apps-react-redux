344a50c6278cef53fa750bd167d1b960e3b6bb91
Jason Hargrove <JHargrove1@humana.com>
2022-02-04 12:52:39 -0500

    Added module rules.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,11 +22,23 @@ module.exports = {
     headers: { "Access-Control-Allow-Origin": "*" },
     https: false,
   },
-  plugins: {
-      new HtmlWebpackPlugin({
-          template: "src/index.html",
-          favicon: "src/favicon.ico"
-      })
-
-  }
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
